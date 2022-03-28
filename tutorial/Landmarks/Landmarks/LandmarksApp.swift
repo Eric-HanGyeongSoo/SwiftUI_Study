3d930d18efcb6b21c65194702e804aec8ceed031
CodeCamper <gksrudtn99@gmail.com>
2022-03-28 12:41:31 +0900

    Creating and Combining Views

---
+++ tutorial/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by baro on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
